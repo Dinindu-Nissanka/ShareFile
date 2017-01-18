3866f0f7a5dfed0c928c853c49dcd867b6eadbd0
Dinindu-Nissanka <dmmnissanka@gmail.com>
2017-01-18 18:04:04 +0530

    Implement ERROR response and LEAVE request

--- src/Node.java
+++ src/Node.java
@@ -20,11 +20,14 @@ import java.util.StringTokenizer;
 import java.util.regex.*;
 
 
+/**
+ * Node class
+ */
 public class Node implements Runnable {
 	
 	private int node_port = 50000;
 	private String node_username = "dinindu";
-	private String node_ip = "127.0.0.1";
+	private String node_ip = null;
 	private RoutingTable node_routing_table = new RoutingTable();
 	private Random ran = new Random();
 	private int file_number = 0;
@@ -34,8 +37,17 @@ public class Node implements Runnable {
 	
 	private DatagramSocket socket = null;
 
-	public Node() throws SocketException {
+    /**
+     * Constructor
+     * Random port will be selected and new node will be created
+     *
+     * @throws SocketException
+     * @throws UnknownHostException
+     */
+	public Node() throws SocketException, UnknownHostException {
 		// TODO Auto-generated constructor stub
+        InetAddress nodeAddress = InetAddress.getLocalHost();
+        node_ip = nodeAddress.getHostAddress();
 		this.node_port = ran.nextInt(10000) + 50000;
 		socket = new DatagramSocket(this.node_port);
 		System.out.println("Node started. Host - "+this.node_ip+", Port - "+this.node_port);
@@ -44,9 +56,13 @@ public class Node implements Runnable {
 		System.out.println("Related text file for this node is "+ file_number+".txt");
 
 		this.searchQueryBuffer = new String[10];
-		this.handleQuerySearch(socket);
+		this.handleQuerySearchInput(socket);
 	}
-	
+
+    /**
+     * Run method
+     * Method will handle the requested command
+     */
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -91,7 +107,12 @@ public class Node implements Runnable {
             	break;
             	
             case "LEAVE":
-            	break;
+                try {
+                    response = handleLEAVERequest(socket,st);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                break;
             	
             case "SER":
 				try {
@@ -102,15 +123,74 @@ public class Node implements Runnable {
 				break;
             	
             default:
-            	response = "ERROR";
-    			response = String.format("%04d", response.length() + 5) + " " + response;
+                try {
+                    handleERRORResponse(incoming,socket,response);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
             }
-            
-            
 		}
 	}
 
-	public String handleSERRequest(DatagramPacket received, DatagramSocket socket, StringTokenizer st) throws FileNotFoundException, IOException {
+    /**
+     * Method to handle Error reponse
+     *
+     * @param incoming
+     * @param socket
+     * @param response
+     * @throws UnknownHostException
+     */
+    private void handleERRORResponse(DatagramPacket incoming, DatagramSocket socket, String response) throws UnknownHostException {
+        response = "ERROR";
+        response = String.format("%04d", response.length() + 5) + " " + response;
+        String ip = incoming.getAddress().getHostAddress();
+        InetAddress ip_address = InetAddress.getByName(ip);
+        DatagramPacket searchReply = new DatagramPacket(response.getBytes() , response.getBytes().length , ip_address , incoming.getPort());
+        try {
+            socket.send(searchReply);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method to handle Leave request
+     *
+     * @param socket
+     * @param st
+     * @return
+     * @throws UnknownHostException
+     */
+    private String handleLEAVERequest(DatagramSocket socket, StringTokenizer st) throws UnknownHostException {
+        String ip_address = st.nextToken();
+        String port = st.nextToken();
+        String response = "LEAVEOK ";
+        boolean result = node_routing_table.removeNeighbour(ip_address,Integer.parseInt(port));
+        if(result)
+        {
+            response = response + "0";
+            response = String.format("%04d", response.length() + 5) + " " + response;
+            DatagramPacket searchReply = new DatagramPacket(response.getBytes() , response.getBytes().length , InetAddress.getByName(ip_address) , Integer.parseInt(port));
+            try {
+                socket.send(searchReply);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Method to handle Search request
+     *
+     * @param received
+     * @param socket
+     * @param st
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public String handleSERRequest(DatagramPacket received, DatagramSocket socket, StringTokenizer st) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
         String ip_address = st.nextToken();
         String pPort = st.nextToken();
@@ -125,8 +205,6 @@ public class Node implements Runnable {
 		int hop_count = Integer.parseInt(hops);
         int ttl_hops = Integer.parseInt(ttl);
         String searchQueryString = ip_address+" "+pPort+" "+file_name+" "+hops;
-
-        printQueryBuffer();
 
         if((isQueryInBuffer(searchQueryString) == 1)||(isQueryInBuffer(searchQueryString) == 2))
         {
@@ -146,18 +224,11 @@ public class Node implements Runnable {
         return null;
     }
 
-    private void printQueryBuffer() {
-	    System.out.println("************************************");
-	    for(int i=0;i<searchQueryBuffer.length;i++)
-	    {
-	        if(searchQueryBuffer[i]!=null)
-	        {
-	            System.out.println(searchQueryBuffer[i]);
-            }
-        }
-        System.out.println("************************************");
-    }
-
+    /**
+     * Method to add search query to the node query buffer
+     *
+     * @param searchQueryString
+     */
     private void addQueryToBuffer(String searchQueryString)
     {
         int index = queryBufferCount % 10;
@@ -165,6 +236,19 @@ public class Node implements Runnable {
         queryBufferCount++;
     }
 
+    /**
+     * Method to forward the query to its neighbours
+     *
+     * @param received
+     * @param socket
+     * @param address
+     * @param port
+     * @param file_name
+     * @param hop_count
+     * @param ttl_hops
+     * @return
+     * @throws UnknownHostException
+     */
     private String forwardSERQuery(DatagramPacket received, DatagramSocket socket, InetAddress address, int port,
                                  String file_name, int hop_count, int ttl_hops) throws UnknownHostException {
         if(ttl_hops<=0)
@@ -199,6 +283,19 @@ public class Node implements Runnable {
         return response;
     }
 
+    /**
+     * Method to check if the file is in node's content
+     *
+     * @param socket
+     * @param address
+     * @param port
+     * @param file_name
+     * @param hop_count
+     * @param ttl_hops
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
     private boolean checkFileExistency(DatagramSocket socket, InetAddress address, int port, String file_name,
                                        int hop_count, int ttl_hops) throws FileNotFoundException, IOException{
 
@@ -244,29 +341,12 @@ public class Node implements Runnable {
 	    return true;
     }
 
-	/*
-    private int isHopCountSmaller(String ipaddress, String pPort, String file_name, String hops)
-    {
-        for(int i=0;i<searchQueryBuffer.length;i++)
-        {
-            StringTokenizer stQuery = new StringTokenizer(searchQueryBuffer[i], " ");
-            String queryIP = stQuery.nextToken();
-            String queryPort = stQuery.nextToken();
-            String queryFileName = stQuery.nextToken();
-            String queryHops = stQuery.nextToken();
-
-            if((queryIP.equals(ipaddress))&&(queryPort.equals(pPort))&&(queryFileName.equals(file_name)))
-            {
-            	if(Integer.parseInt(queryHops) < Integer.parseInt(hops))
-            		return 1;
-            	else
-            		return 0;
-			}
-        }
-        return 2;
-    }
-    */
-
+    /**
+     * Method to check if the query is an already received one
+     *
+     * @param searchQueryString
+     * @return
+     */
     private int isQueryInBuffer(String searchQueryString)
     {
         StringTokenizer st = new StringTokenizer(searchQueryString," ");
@@ -307,6 +387,14 @@ public class Node implements Runnable {
         return 0;
     }
 
+    /**
+     * Method to handle Join request
+     *
+     * @param socket
+     * @param st
+     * @return
+     * @throws UnknownHostException
+     */
 	public String handleJOINRequest(DatagramSocket socket, StringTokenizer st) throws UnknownHostException
 	{
         String ip_address = st.nextToken();
@@ -338,8 +426,13 @@ public class Node implements Runnable {
         
         return response;
 	}
-	
-	public void registerServer() throws IOException
+
+    /**
+     * Method to register with the Bootstrap server
+     *
+     * @throws IOException
+     */
+    public void registerServer() throws IOException
 	{
 		int response_type = 9999;
 		
@@ -400,8 +493,16 @@ public class Node implements Runnable {
 		}
 
 	}
-	
-	public void joinNeighbours(int no_of_neighbours, StringTokenizer st, DatagramSocket socket) throws UnknownHostException
+
+    /**
+     * Method to check the initial neighbour count of the node
+     *
+     * @param no_of_neighbours
+     * @param st
+     * @param socket
+     * @throws UnknownHostException
+     */
+    public void joinNeighbours(int no_of_neighbours, StringTokenizer st, DatagramSocket socket) throws UnknownHostException
 	{   
         if(no_of_neighbours == 1)
         {
@@ -422,8 +523,18 @@ public class Node implements Runnable {
         	System.out.println(response_neighbour_2);
         }
 	}
-	
-	public String joinNeighbour(String neighbour_ip,String port, DatagramSocket socket) throws UnknownHostException
+
+
+    /**
+     * Method to send a JOIN request to a neighbour node
+     *
+     * @param neighbour_ip
+     * @param port
+     * @param socket
+     * @return
+     * @throws UnknownHostException
+     */
+    public String joinNeighbour(String neighbour_ip,String port, DatagramSocket socket) throws UnknownHostException
 	{
     	InetAddress neighbour_address = InetAddress.getByName(neighbour_ip);
     	int neighbour_port = Integer.parseInt(port);
@@ -473,8 +584,13 @@ public class Node implements Runnable {
     	}
 		return return_string;
 	}
-	
-	public void handleQuerySearch(final DatagramSocket socket)
+
+    /**
+     * Thread to get the user input for the search query
+     *
+     * @param socket
+     */
+    public void handleQuerySearchInput(final DatagramSocket socket)
 	{
 		Thread t = new Thread() {
             private Scanner scanner;
